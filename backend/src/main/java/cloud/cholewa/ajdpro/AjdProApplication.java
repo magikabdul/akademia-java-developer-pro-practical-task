499dd50cbed0ae9c81f9cb6dfcda920a04951615
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-02-10 23:37:59 +0100

    [AJD-0] initial backend commit

---
+++ backend/src/main/java/cloud/cholewa/ajdpro/AjdProApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.ajdpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjdProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjdProApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cloud/cholewa/ajdpro/AjdProApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.ajdpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjdProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
